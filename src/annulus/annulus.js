a010b65e4e9c23a7033c327686cd6a0decceba8d
Micky Faas <micky@edukitty.org>
2019-06-03 12:12:42 +0200

    And it moves!

--- src/annulus/annulus.js
+++ src/annulus/annulus.js
@@ -29,8 +29,12 @@ class Annulus {
 
     constructor( svgarea  ) {
         this._palette = [ COLOR0, COLOR1, COLOR2 ];
-        this._excitement = 0.0;
+        this._stress = 0.0;
         this._showDebug = true;
+        this._frame =0;
+        this._time =0;
+        this._speed =0.000005;
+        this._animate =false;
         this.svgarea = svgarea;
 
         svgarea.viewbox = [-2,-2,4,4];
@@ -55,26 +59,53 @@ class Annulus {
     get color2()        { return this._palette[2]; }
     set color2(c)       { this._palette[2] =c; this.update(); }
 
-    get excitement()    { return this._excitement; }
-    set excitement(e)   { this._excitement = e; this.update(); }
+    get stress()    { return this._stress; }
+    set stress(e)   { this._stress = e; this.update(); }
 
     get showDebug()     { return this._showDebug; }
     set showDebug(b)    { this._showDebug =b; this.update(); }
 
+    get frame()         { return this._frame; }
+    set frame(f)        { this._frame =f; this.update(); }
+
+    get animate()       { return this._animate; }
+    set animate(b)      { this._animate = b; this.start(); }
+
     update() {
-        this._path.setAttributeNS(null, 'fill', this.rgbToHex( this.excitementToColor( this._excitement ) ) );
+        this._path.setAttributeNS(null, 'fill', this.rgbToHex( this.stressToColor( this._stress ) ) );
         this._path.setAttributeNS(null, 'd', this.createPath() );
     }
 
-    excitementToColor( ex ) {
+    start() {
+        if( this._animate == false ) return;
+
+        const animateStep = (timestamp) => {
+            if( this._animate == false ) return;
+            if( !this._time ) this._time =timestamp;
+            let delta = this._time - timestamp;
+            
+            this._frame += delta * this._speed;
+            while( this._frame >= 1.0 ) this._frame -= 1.0;
+            
+            this._time = timestamp;
+            this.update();
+            window.requestAnimationFrame( animateStep );
+        }
+        window.requestAnimationFrame( animateStep );
+    }
+
+    stressToColor( stress ) {
         // Linearly interpolate over the colors in _palette using .5 as midpoint
-        let scaled = ex / EXCITEMENT_SCALE;
+        let scaled = stress / EXCITEMENT_SCALE;
 
         return (scaled < .5)
             ? this.colorLerp( this._palette[0], this._palette[1], scaled * 2.0 )
             : this.colorLerp( this._palette[1], this._palette[2], (scaled-.5) * 2.0 );
     }
 
+    /* Given two rgb arrays [r,g,b] @c1 and @c2,
+     * linearly interpolate between @c1 and @c2 at @x 
+     */
     colorLerp( c1, c2, x ) {
         return [
             Math.floor(c2[0] * x + c1[0] * (1.0-x)),
@@ -82,16 +113,28 @@ class Annulus {
             Math.floor(c2[2] * x + c1[2] * (1.0-x)) ];
     }
 
+    /* Given an array of rgb values @rgb, return its HTML hex notation
+     */
     rgbToHex( rgb ) {
         return '#' + rgb[0].toString(16) + rgb[1].toString(16) + rgb[2].toString(16);
     }
 
+    /* Divides a sinewave 'oscillator' into @n segments, 
+     * then returns from the @i-th segment the value @frame on the interval [0;1)
+     * The returned value is on the interval [0;1]
+     */
+    oscillator( n, i, frame ) {
+        const period = Math.PI * (n*.789);
+        return (Math.sin( (i/n + frame) * period ) + 1.0) / 2.0; 
+    }
+
     createPath() {
         const r = 1.0;
-        const in_segs = 6; // Number of inner segments
-        const out_segs = 6; // Number of outer segments
+        const out_segs = 40; // Number of inner segments
+        const in_segs = 40; // Number of outer segments
         const thickness = 0.5; // Thickness of the ring
         const t = r * (1.0-thickness);
+        const s = Math.pow( this._stress, 2 ) + 0.05;
 
         // Clear out the debug stuff
         while( this._debugg.lastChild ) this._debugg.removeChild( this._debugg.lastChild );
@@ -99,80 +142,131 @@ class Annulus {
         let p = "";
         
         let theta =0;
-        let theta_step =(Math.PI*2)/in_segs;
-        let hdist =(4/3)*Math.tan( Math.PI/(in_segs*2) );
+        let theta_step =(Math.PI*2)/out_segs;
+        let hdist =(4/3)*Math.tan( Math.PI/(out_segs*2) );
+        
+        let prng = 123456789; // Seed for prng
+        let next_theta = function() {
+                prng ^= ( prng << 13 );
+                prng ^= ( prng >>> 7 );
+                prng ^= ( prng << 17 );
+                let q =prng / (-1>>>0) + 1.0;
+                //console.log( q );
+                return theta + theta_step * q;
+            };
 
-        for( let i =0 ; i <= in_segs; i++ ) {
+        //for( let i =0 ; i <= out_segs; i++ ) {
             
-            let re =r+this._excitement/(r*2.0);
+        let prev_px =0; let prev_py =0;
 
-            let cx1=(Math.cos( theta ) - hdist * Math.sin( theta )) * r;
-            let cy1=(Math.sin( theta ) + hdist * Math.cos( theta )) * r;
+        for( let i =0; i <= out_segs; i++) {
             
-            theta += theta_step;
+            let osc =this.oscillator( out_segs, i % out_segs, this._frame);  
+            let re =r + (osc+.5)  * s / (r*4);
+            let re1=r + (osc+.5)  * s / (r*16);
+            let re2=r + (1-osc+.5)* s / (r*16);
+            
+            let prev_theta =theta;
+            theta += theta_step * (1-osc+.49);
+            //theta =next_theta();
+
+            if( i == out_segs ) theta = 2*Math.PI;
+
+            hdist =(4/3)*Math.tan( (theta-prev_theta)/4 );
+
             let px =Math.cos( theta ) * re;
             let py =Math.sin( theta ) * re;
+            
+            let cx1=(Math.cos( prev_theta ) - hdist * Math.sin( prev_theta )) * re1;
+            let cy1=(Math.sin( prev_theta ) + hdist * Math.cos( prev_theta )) * re1;
 
-            let cx2=(Math.cos( theta ) + hdist * Math.sin( theta  )) * r;
-            let cy2=(Math.sin( theta ) - hdist * Math.cos( theta  )) * r;
+            let cx2=(Math.cos( theta ) + hdist * Math.sin( theta  )) * re2;
+            let cy2=(Math.sin( theta ) - hdist * Math.cos( theta  )) * re2;
             
-            if( i == 0 )
-                p += " M" + px.toFixed(5) + "," + py.toFixed(5);
-            else
-                p += " C" 
-                    + cx1.toFixed(5) + "," + cy1.toFixed(5) + " " 
-                    + cx2.toFixed(5) + "," + cy2.toFixed(5) + " " 
-                    + px.toFixed(5) + "," + py.toFixed(5);
+            if( i == 0 ) {
+                p += " M" + 1*re + "," + 0*re;
+                prev_px = 1*re; prev_py = 0*re;
+            }
+            p += " C" 
+                + cx1.toFixed(5) + "," + cy1.toFixed(5) + " " 
+                + cx2.toFixed(5) + "," + cy2.toFixed(5) + " " 
+                + px.toFixed(5) + "," + py.toFixed(5);
             
-            if( this._showDebug == false ) continue;
+            if( this._showDebug ) {
 
-            // Only boring debug-stuffs below
+                // Only boring debug-stuffs below
 
-            let l1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-            l1.setAttributeNS(null, 'stroke-width', '0.002');
-            l1.setAttributeNS(null, 'stroke', 'white');
-            l1.setAttributeNS(null, 'd', "M"  
-                    + cx2.toFixed(5) + "," + cy2.toFixed(5) + " L" 
-                    + px.toFixed(5) + "," + py.toFixed(5) + " M"
-                    + cx1.toFixed(5) + "," + cy1.toFixed(5) + " L" 
-                    + (Math.cos(theta-theta_step)*re).toFixed(5) + "," + (Math.sin(theta-theta_step)*re).toFixed(5) + " Z" ); 
-            this._debugg.appendChild(l1);
-            
-            let h1 = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-            h1.setAttributeNS(null, 'fill', 'magenta');
-            h1.setAttributeNS(null, 'width', '0.02');
-            h1.setAttributeNS(null, 'height', '0.02');
-            h1.setAttributeNS(null, 'x', cx1.toFixed(5)-0.01 );
-            h1.setAttributeNS(null, 'y', cy1.toFixed(5)-0.01 );
-            this._debugg.appendChild(h1);
-            
-            let h2 = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-            h2.setAttributeNS(null, 'fill', 'cyan');
-            h2.setAttributeNS(null, 'width', '0.02');
-            h2.setAttributeNS(null, 'height', '0.02');
-            h2.setAttributeNS(null, 'x', cx2.toFixed(5)-0.01 );
-            h2.setAttributeNS(null, 'y', cy2.toFixed(5)-0.01 );
-            this._debugg.appendChild(h2);
-            
-            let c = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-            c.setAttributeNS(null, 'fill', 'red');
-            c.setAttributeNS(null, 'r', '0.01');
-            c.setAttributeNS(null, 'cx', px.toFixed(5) );
-            c.setAttributeNS(null, 'cy', py.toFixed(5) );
-            this._debugg.appendChild(c);
+                let l1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+                l1.setAttributeNS(null, 'stroke-width', '0.002');
+                l1.setAttributeNS(null, 'stroke', 'white');
+                l1.setAttributeNS(null, 'd', "M"  
+                        + cx2.toFixed(5) + "," + cy2.toFixed(5) + " L" 
+                        + px.toFixed(5) + "," + py.toFixed(5) + " M"
+                        + cx1.toFixed(5) + "," + cy1.toFixed(5) + " L" 
+                        + prev_px.toFixed(5) + "," + prev_py + " Z" ); 
+                this._debugg.appendChild(l1);
+                
+                let h1 = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+                h1.setAttributeNS(null, 'fill', 'magenta');
+                h1.setAttributeNS(null, 'width', '0.02');
+                h1.setAttributeNS(null, 'height', '0.02');
+                h1.setAttributeNS(null, 'x', cx1.toFixed(5)-0.01 );
+                h1.setAttributeNS(null, 'y', cy1.toFixed(5)-0.01 );
+                this._debugg.appendChild(h1);
+                
+                let h2 = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+                h2.setAttributeNS(null, 'fill', 'cyan');
+                h2.setAttributeNS(null, 'width', '0.02');
+                h2.setAttributeNS(null, 'height', '0.02');
+                h2.setAttributeNS(null, 'x', cx2.toFixed(5)-0.01 );
+                h2.setAttributeNS(null, 'y', cy2.toFixed(5)-0.01 );
+                this._debugg.appendChild(h2);
+                
+                let c = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+                c.setAttributeNS(null, 'fill', 'red');
+                c.setAttributeNS(null, 'r', '0.01');
+                c.setAttributeNS(null, 'cx', px.toFixed(5) );
+                c.setAttributeNS(null, 'cy', py.toFixed(5) );
+                this._debugg.appendChild(c);
+            }
+
+            prev_px =px; prev_py =py;
+
         }
         
         theta =0;
-        theta_step =-(Math.PI*2)/out_segs;
+        theta_step =-(Math.PI*2)/in_segs;
 
-        for( let i =0 ; i <= out_segs; i++ ) {
-            theta += theta_step;
-            let px =Math.cos( theta ).toFixed(5) * t;
-            let py =Math.sin( theta ).toFixed(5) * t;
-            if( i == 0 )
-                p += " M" + px + "," + py;
-            else
-                p += " L" + px + "," + py;
+        for( let i =0 ; i <= in_segs; i++ ) {
+            let osc =this.oscillator( in_segs, i % in_segs, this._frame);  
+            let re =t;// + (osc)  * 0.02
+            let re1=t;// + (osc)  * 0.01
+            let re2=t;// + (1-osc)* 0.01
+            
+            let prev_theta =theta;
+            theta += theta_step * (1-osc+.49);
+
+            if( i == in_segs ) theta = 2*Math.PI;
+
+            hdist =(4/3)*Math.tan( (theta-prev_theta)/4 );
+
+            let px =Math.cos( theta ) * re;
+            let py =Math.sin( theta ) * re;
+            
+            let cx1=(Math.cos( prev_theta ) - hdist * Math.sin( prev_theta )) * re1;
+            let cy1=(Math.sin( prev_theta ) + hdist * Math.cos( prev_theta )) * re1;
+
+            let cx2=(Math.cos( theta ) + hdist * Math.sin( theta  )) * re2;
+            let cy2=(Math.sin( theta ) - hdist * Math.cos( theta  )) * re2;
+            
+            if( i == 0 ) {
+                p += " M" + 1*re + "," + 0*re;
+                prev_px = 1*re; prev_py = 0*re;
+            }
+            p += " C" 
+                + cx1.toFixed(5) + "," + cy1.toFixed(5) + " " 
+                + cx2.toFixed(5) + "," + cy2.toFixed(5) + " " 
+                + px.toFixed(5) + "," + py.toFixed(5);
         }
 
 
@@ -206,7 +300,7 @@ class App {
         this.toolbox = new dat.GUI( );
         
         let f_a = this.toolbox.addFolder( 'Annulus' );
-        f_a.add( this.annulus, 'excitement', 0.0, EXCITEMENT_SCALE ).name( 'Excitement' ).step( 0.01 * EXCITEMENT_SCALE );
+        f_a.add( this.annulus, 'stress', 0.0, EXCITEMENT_SCALE ).name( 'Stress' ).step( 0.01 * EXCITEMENT_SCALE );
         f_a.open();
 
         // Colors toolbox
@@ -216,6 +310,12 @@ class App {
         f_c.addColor( this.annulus, 'color2' ).name( 'High' );
 //        f_c.addColor( this.viewport, 'backgroundColor' ).name( 'Background' );
         f_c.open();
+        
+        let f_p = this.toolbox.addFolder( 'Parameters' );
+        f_p.add( this.annulus, 'showDebug' ).name( 'Debug' );
+        //f_p.add( this.annulus, 'frame', 0.0, 1.0 ).name( 'Frame' ).step( 0.01);
+        f_p.add( this.annulus, 'animate' ).name( 'Animate' );
+        f_p.open();
 
     }
 }
